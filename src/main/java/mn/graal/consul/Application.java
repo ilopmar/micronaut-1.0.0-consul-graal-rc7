592fcbb17bc0d449089434cc252c48184e35d8d1
Iván López <lopez.ivan@gmail.com>
2018-10-31 09:58:24 +0100

    Initial commit

---
+++ src/main/java/mn/graal/consul/Application.java
@@ -0,0 +1,10 @@
+package mn.graal.consul;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class);
+    }
+}
